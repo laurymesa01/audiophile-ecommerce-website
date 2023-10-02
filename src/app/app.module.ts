002a93d6c42880bdf4dd5e0074313b98959badbe
laurymesa01 <mesafernandezlauraelena@gmail.com>
2023-10-02 23:13:15 +0200

    Laying out the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'dark-orange': '#D87D4A',
+      'light-orange': '#fbaf85',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'dark-grey': '#F1F1F1',
+      'light-grey': '#FAFAFA',
+      'almost-black': '#101010'
+    }
   },
   plugins: [],
 }
