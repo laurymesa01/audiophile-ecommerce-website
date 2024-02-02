0a73a51886fffa0dba06605bd97dfea0eff677d7
laurymesa01 <mesafernandezlauraelena@gmail.com>
2024-02-02 21:15:44 +0100

    Working on bill component

--- src/app/components/bill/bill.component.ts
+++ src/app/components/bill/bill.component.ts
@@ -12,11 +12,13 @@ export class BillComponent implements OnInit{
   @Input() cart: Cart[] = [];
   @Input() grandTotal: number = 0;
   product!: Cart;
+  items: number = 0;
 
   constructor(private router: Router){}
 
   ngOnInit(){
     this.product = this.cart[0];
+    this.items = this.cart.length - 1;
   }
 
   backToHome(){
